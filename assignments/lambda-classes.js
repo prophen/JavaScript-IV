54a35ca4dfb83a1c0a2ac93a798dd9ca3a646523
Nikema Prophet <nikema@gmail.com>
2019-06-13 13:08:53 -0700

    Finished class definitions with ProjectManager class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -109,3 +109,32 @@ class Student extends Person {
     console.log(`${this.name} has begun a sprint challenge on ${subject}`);
   }
 }
+
+/* 
+#### Project Manager
+
+* Now that we have instructors and students, we'd be nowhere without our PM's
+* ProjectManagers are extensions of Instructors
+* ProjectManagers have the following unique props:
+  * `gradClassName`: i.e. CS1
+  * `favInstructor`: i.e. Sean
+* ProjectManagers have the following Methods:
+  * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+  * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
+*/
+
+class ProjectManager extends Instructor {
+  constructor(props) {
+    super(props)
+    this.gradClassName = props.gradClassName
+    this.favInstructor = props.favInstructor
+  }
+
+  standup(channel) {
+    console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)
+  }
+
+  debugsCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+  }
+}
